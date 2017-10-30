e69b5756065ea232c17b6699198053a624b5a0a6
devxris <chuwei0915@gmail.com>
2017-10-30 14:36:06 +0800

    UI Setup for GoalsVC

---
+++ Goal/Controller/GoalsVC.swift
@@ -0,0 +1,19 @@
+//
+//  GoalsVC.swift
+//  Goal
+//
+//  Created by Chris Huang on 30/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+	// outlets
+	@IBOutlet weak var goalsTable: UITableView!
+	
+	// target actions
+	@IBAction func addGoal(_ sender: UIButton) {
+	}
+}
