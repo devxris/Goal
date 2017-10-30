0ae2b919855357375f174edc9e74c6d8d28f7203
devxris <chuwei0915@gmail.com>
2017-10-30 14:58:33 +0800

    setup GoalCell

---
+++ Goal/Controller/GoalCell.swift
@@ -0,0 +1,22 @@
+//
+//  GoalCell.swift
+//  Goal
+//
+//  Created by Chris Huang on 30/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+	@IBOutlet weak var goalDescription: UILabel!
+	@IBOutlet weak var goalType: UILabel!
+	@IBOutlet weak var goalProgress: UILabel!
+	
+	func configure(description: String, type: String, progress: Int) {
+		goalDescription.text = description
+		goalType.text = type
+		goalProgress.text = String(progress)
+	}
+}
